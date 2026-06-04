e0dd4b38d6e716fe068c465c4bbe31e272b883f4
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-06-04 23:47:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+	square.addEventListner("mouseenter", () => {
+		squares.forEach((s) => {
+			if(s !== square) {
+				s.style.backgroundColor = "#6F4E37";
+			} else {
+				s.style.backgroundColor = "#E6E6FA";
+			}
+		});
+	});
+
+	square.addEventListener("mouseleave", () => {
+		squares.forEach((s) => {
+			s.style.backgroundColor = "E6E6FA";
+		})
+	})
+})
